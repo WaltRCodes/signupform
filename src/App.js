7bc3b0d112cd50c05dd56616ab2e8386b7990a3b
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-09 17:04:16 -0400

    created state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     
-    
+    this.state = {
+      username: '',
+      password: '',
+      age: 0,
+      gender: '',
+      occupation: '' 
+    }
   }
   
   render() {
