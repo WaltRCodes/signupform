4eb39fef5a7d3d115c7b5bcc9b6a30e6c169990f
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-10 11:43:20 -0400

    created functions

--- src/App.js
+++ src/App.js
@@ -12,8 +12,33 @@ export default class App extends Component {
       gender: '',
       occupation: '' 
     }
+
+    this.createComponent = this.createComponent.bind(this);
+    this.takeName = this.takeName.bind(this);
+    this.takePassword = this.takePassword.bind(this);
+    this.takeAge = this.takeAge.bind(this);
+    this.takeGender = this.takeGender.bind(this);
+    this.takeOccupation = this.takeOccupation.bind(this);
   }
   
+  createComponent(event){
+  }
+
+  takeName(event){
+  }
+
+  takePassword(event){
+  }
+
+  takeAge(event){
+  }
+
+  takeGender(event){
+  }
+
+  takeOccupation(event){
+  }
+
   render() {
     return (
       <div>
