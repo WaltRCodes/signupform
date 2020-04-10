1c6a881eec7bfcbdce15d1214d7dc3a08df5100b
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-10 14:10:26 -0400

    updated the info component

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 export default function Info(props) {
     return (
         <div>
-
+            <p>{props.username}</p>
+            <p>{props.password}</p>
+            <p>{props.age}</p>
+            <p>{props.gender}</p>
+            <p>{props.occupation}</p>
         </div>
     );
 }
